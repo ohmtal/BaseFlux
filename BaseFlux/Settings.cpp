c738ae9b0996c4992521a7fe0191420494b5c921
XXTH <tom@mxplus.de>
2026-08-20 05:40:11 +0200

    fixed memleak / SDL_GetPrefsPath

--- BaseFlux/Settings.cpp
+++ BaseFlux/Settings.cpp
@@ -6,8 +6,9 @@ namespace BaseFlux {
     std::string Settings::getPrefsPath(){
         static std::string cachedPath = "";
         if (!cachedPath.empty()) return cachedPath;
-        const char* rawPath = SDL_GetPrefPath(getSafeCompany().c_str(), getSafeCaption().c_str());
+        char* rawPath = SDL_GetPrefPath(getSafeCompany().c_str(), getSafeCaption().c_str());
         cachedPath = rawPath ? std::string(rawPath) : "";
+        if (rawPath) SDL_free(rawPath);
         return cachedPath;
     }
     //--------------------------------------------------------------------------
